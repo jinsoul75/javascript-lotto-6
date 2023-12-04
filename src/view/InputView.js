14824219b85320cd779037ca2f35b365a814d395
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-12-04 15:46:39 +0900

    fix: 비동기 함수 await 키워드 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,19 +3,23 @@ import { MESSAGES } from '../constants/messages.js';
 
 const InputView = {
   async readAmount() {
-    const amount = Console.readLineAsync(MESSAGES.inputAmount);
+    const amount = await Console.readLineAsync(MESSAGES.inputAmount);
     return amount;
   },
 
   async readLottoNumber() {
-    const userLottoNumber = Console.readLineAsync(MESSAGES.askLottoNumber);
+    const userLottoNumber = await Console.readLineAsync(
+      MESSAGES.askLottoNumber,
+    );
     return userLottoNumber;
   },
 
-  async readBonusNumber(){
-    const userBonusNumber = Console.readLineAsync(MESSAGES.askBonusNumber);
-    return userBonusNumber
-  }
+  async readBonusNumber() {
+    const userBonusNumber = await Console.readLineAsync(
+      MESSAGES.askBonusNumber,
+    );
+    return userBonusNumber;
+  },
 };
 
 export default InputView;
